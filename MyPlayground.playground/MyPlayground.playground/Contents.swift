94b2d51e49ceea36157f2a78055a6591846eb1f4
Paulo Andres <110691699+andrvss@users.noreply.github.com>
2023-12-25 18:31:24 -0300

    initial commit

---
+++ MyPlayground.playground/MyPlayground.playground/Contents.swift
@@ -0,0 +1,75 @@
+class Conta {
+    var saldo = 0.0
+    var nome: String
+    
+    func sacar(_ valor: Double) {
+        saldo -= valor
+    }
+
+    func depositar(_ valor: Double) {
+        saldo += valor
+    }
+
+    init(nome: String) {
+        self.nome = nome
+    }
+}
+
+class ContaPoupanca: Conta {
+    var possuiCartaoDebito: Bool
+    func solicitarDebito() {
+        possuiCartaoDebito = true
+        print("O cliente esta solicitando cartao de debito")
+    }
+    
+    override func sacar(_ valor: Double) {
+        saldo -= valor + 10
+    }
+    
+    init(nome: String, possuiCartaoDebito: Bool) {
+        self.possuiCartaoDebito = possuiCartaoDebito
+        super.init(nome: nome)
+    }
+}
+
+var contaPoupanca = ContaPoupanca(nome: "Paulo", possuiCartaoDebito: false)
+contaPoupanca.depositar(50)
+contaPoupanca.sacar(20)
+print(contaPoupanca.saldo)
+contaPoupanca.solicitarDebito()
+
+class ContaCorrente: Conta {
+    func solicitarEmprestimo(_ valor: Double) {
+        print("O cliente esta solicitando um emprestimo no valor de \(valor)")
+        super.depositar(valor)
+    }
+    
+    override func sacar(_ valor: Double) {
+        saldo -= valor + 5
+    }
+}
+
+var contaCorrente = ContaCorrente(nome: "Cesar")
+contaCorrente.depositar(100)
+contaCorrente.sacar(20)
+print(contaCorrente.saldo)
+print(contaCorrente.solicitarEmprestimo(2000))
+print(contaCorrente.saldo)
+
+func exibeSaldoDaConta(_ conta: Conta) {
+    if conta is ContaCorrente {
+        print("Conta e do tipo conta corrente")
+    }
+    
+    if let contaCorrente = conta as? ContaCorrente {
+        contaCorrente.solicitarEmprestimo(200)
+    }
+    
+    guard let contaPoupanca = conta as? ContaPoupanca else { return
+        print(contaPoupanca.possuiCartaoDebito)
+    }
+}
+
+exibeSaldoDaConta(contaCorrente)
+exibeSaldoDaConta(contaPoupanca)
+
